e83362ca9a6841729ee44876fae216e312de2105
cbgriffith <c.b.griffith@outlook.com>
2021-10-15 10:58:15 -0400

    cleaned up my main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,7 @@
-import { Fish } from './fish/Fish.js'
-import { useFish } from './fish/FishDataProvider.js'
 import { FishList } from './fish/FishList.js'
-import { Location } from './locations/Location.js'
-import { useLocation } from './locations/LocationDataProvider.js'
 import { LocationList } from './locations/LocationList.js'
-import { Tip } from './tips/Tip.js'
-import { useTip } from './tips/TipDataProvider.js'
 import { TipList } from './tips/TipList.js'
-const allTheFish = useFish()
 
-for (const fish of allTheFish) {
-    console.log(fish)
-}
 
 FishList()
 LocationList()
