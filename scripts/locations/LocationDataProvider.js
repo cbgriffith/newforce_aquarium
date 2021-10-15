3b674d8c60b8ee74607ce9ee8c2c96afec6fa71f
cbgriffith <c.b.griffith@outlook.com>
2021-10-15 10:53:40 -0400

    automated the tips

--- scripts/locations/LocationDataProvider.js
+++ scripts/locations/LocationDataProvider.js
@@ -3,11 +3,3 @@ const locationCollection = ["New River","Mississippi River","Nile River","Kanawh
 export const useLocation = () => {
     return locationCollection.slice()
 }
-
-// let list = document.getElementById("LocationList");
-
-// locationCollection.forEach((item)=>{
-//   let li = document.createElement("li");
-//   li.innerText = item;
-//   list.appendChild(li);
-// })
--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ import { FishList } from './fish/FishList.js'
 import { Location } from './locations/Location.js'
 import { useLocation } from './locations/LocationDataProvider.js'
 import { LocationList } from './locations/LocationList.js'
+import { Tip } from './tips/Tip.js'
+import { useTip } from './tips/TipDataProvider.js'
+import { TipList } from './tips/TipList.js'
 const allTheFish = useFish()
 
 for (const fish of allTheFish) {
@@ -12,3 +15,4 @@ for (const fish of allTheFish) {
 
 FishList()
 LocationList()
+TipList()
